f051cadf34792538410d37c0a8500b4b3e9efc41
Johnicholas Hines <johnicholas.hines@gmail.com>
2011-09-09 18:09:17 -0700

    Conceptually, I wrote direct first, and then translated it to trampoline, but in fact it was the other way around. Remove some accidental evidence of this irregularity.

--- direct_terp.pl
+++ direct_terp.pl
@@ -1,8 +1,8 @@
 % -*-Prolog-*-
+%% I am not a prolog programmer, so I will overcomment everything.
 
 % lookup(+ToFind, +Env, -Out)
 %% inefficient linear search of an association list
-%% I am not a prolog programmer, so I will overcomment everything.
 %% If ToFind matches Key, commit to this rule, and bind Out to Value
 lookup(ToFind, extend(Key, Value, _Env), Out) :- ToFind = Key, !, Out = Value .
 %% If ToFind does not match Key, commit to this rule, and recurse deeper.
@@ -18,7 +18,7 @@ ev(if(Test, TrueB, FalseB), Env, Out) :- !, ev(Test, Env, Result), decide(Result
 %% to eval a lambda, bundle the formal, body, and env into a closure and bind it to Out.
 ev(lambda(Formal, Body), Env, Out) :- !, Out = closure(Formal, Body, Env) .
 %% to eval an app, eval the function expression, then eval the arg expression,
-%% then assert that the function really evalled to a closure,
+%% then assert that the function expression evalled to a closure,
 %% and eval the body, in an extended environment that binds the formal parameter
 %% to the value of the argument expression
 ev(app(F, X), E, Out) :- !, ev(F, E, P), ev(X, E, V), P = closure(Formal, B, E), ev(B, extend(Formal, V, E), Out) .
@@ -28,18 +28,6 @@ ev(app(F, X), E, Out) :- !, ev(F, E, P), ev(X, E, V), P = closure(Formal, B, E),
 decide(Result, TrueBranch, _FalseBranch, Env, Out) :- Result = true, !, ev(TrueBranch, Env, Out) .
 %% if the result of the test was false, eval the false branch
 decide(Result, _TrueBranch, FalseBranch, Env, Out) :- Result = false, !, ev(FalseBranch, Env, Out) .
-
-% continue(+Cont, +Val, -Out)
-%% To continue a done, bind Out to Val
-continue(done, Val, Out) :- !, Out = Val .
-%% To continue a decide where the result of the test was true, eval the true branch
-continue(decide(TrueBranch, _FalseBranch, Env, K), Val, Out) :- Val = true, !, ev(TrueBranch, Env, K, Out) .
-%% To continue a decide where the result of the test was false, eval the false branch
-continue(decide(_TrueBranch, FalseBranch, Env, K), Val, Out) :- Val = false, !, ev(FalseBranch, Env, K, Out) .
-%% To continue an arg evaluation, eval the arg and plan to plug the arg val into the proc
-continue(arg(ArgExp, Env, K), Proc, Out) :- !, ev(ArgExp, Env, plug(Proc, K), Out) . 
-%% To continue a plug-in, assert that Proc is indeed a closure, and eval the body in an extended environment
-continue(plug(Proc, K), Actual, Out) :- !, Proc = closure(Formal, Body, E), ev(Body, extend(Formal, Actual, E), K, Out) .
 
 % start(+Exp, -Out)
 %% to start off, use an empty environment
